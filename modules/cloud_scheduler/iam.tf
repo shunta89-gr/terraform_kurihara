228d0c6c3547b21c3735cc74b8b64b0d0b124082
= <=>
2023-09-20 14:01:45 +0900

    SAへのロール割当方法をgoogle_project_iam_memberに変更

--- modules/cloud_scheduler/iam.tf
+++ modules/cloud_scheduler/iam.tf
@@ -1,9 +1,6 @@
-resource "google_project_iam_binding" "scheduler_sa_binding" {
+resource "google_project_iam_member" "scheduler_sa_binding" {
   project = var.project_id
-  count   = length(var.scheduler_sa_roles)
-  role    = var.scheduler_sa_roles[count.index]
-
-  members = [
-    "serviceAccount:${google_service_account.scheduler_sa.email}",
-  ]
+  count   = "${length(var.scheduler_sa_roles)}"
+  role    = "${element(var.scheduler_sa_roles, count.index)}"
+  member = "serviceAccount:${google_service_account.scheduler_sa.email}"
 }
--- modules/cloud_workflows/iam.tf
+++ modules/cloud_workflows/iam.tf
@@ -1,9 +1,6 @@
-resource "google_project_iam_binding" "workflow_sa_binding" {
+resource "google_project_iam_member" "workflow_sa_binding" {
   project = var.project_id
   count   = length(var.workflow_sa_roles)
-  role    = var.workflow_sa_roles[count.index]
-
-  members = [
-    "serviceAccount:${google_service_account.workflow_sa.email}",
-  ]
+  role    = element(var.workflow_sa_roles, count.index)
+  member  = "serviceAccount:${google_service_account.workflow_sa.email}"
 }
