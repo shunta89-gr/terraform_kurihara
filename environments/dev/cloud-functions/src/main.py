d5121f68a2b74f66d5c747264f7b83eafab32c25
阿久津 健 <01044876@Y19MM284.local>
2023-09-20 10:03:46 +0900

    HTTPリクエストでGETまたはPOSTでのパラメータを取得するよう対応

--- environments/dev/cloud-functions/src/main.py
+++ environments/dev/cloud-functions/src/main.py
@@ -2,8 +2,17 @@ import functions_framework
 from rawdata.rawdata import RawData
 
 def import_csv(cloud_event):
-    # target_date = cloud_event.args.get('target_date', '')
     target_date = ''
+    request_args = cloud_event.args
+    request_json = cloud_event.get_json(silent=True)
+    
+    if request_args and 'target_date' in request_args:
+        target_date = request_args.get('target_date')
+    elif request_json and 'target_date' in request_json:
+        target_date = request_json.get('target_date')
+    else:
+        target_date = ''
+    print("---- target_date = {}".format(target_date))
     rawdata = RawData()
     rawdata.exec(target_date)
     return ("Done!\n", 200)
