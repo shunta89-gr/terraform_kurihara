4bffb429e4374e1fa9650dba856ccac72ee73e48
= <=>
2023-09-20 14:12:06 +0900

    SAへの割当ロールを有効なものに変更

--- environments/dev/scheduler.tf
+++ environments/dev/scheduler.tf
@@ -4,10 +4,10 @@ module "cloud_scheduler" {
   region                    = var.region
   job_name                  = "kenkokazoku-job"
   job_schedule              = "0 0 1 * *" #TODO:タイミングは決まり次第変更
-  scheduler_sa_id           = "kenkokazoku-job-executer"
+  scheduler_sa_id           = "kenkokazoku-job-sa"
   scheduler_sa_display_name = "Service Account for Scheduler"
   scheduler_sa_roles = [
-    "roles/workflows.executor"
+    "roles/workflows.invoker"
   ]
   target_workflow_name = "kenkokazoku-workflow"
 }
--- environments/dev/workflow.tf
+++ environments/dev/workflow.tf
@@ -2,11 +2,10 @@ module "cloud_workflow" {
   source                   = "../../modules/cloud_workflows"
   project_id               = var.project_id
   workflow_name            = "kenkokazoku-workflow"
-  workflow_sa_id           = "kenkokazoku-workflow-executer"
+  workflow_sa_id           = "kenkokazoku-workflow-sa"
   workflow_sa_display_name = "Service Account for Workflow"
   workflow_sa_roles = [
     // TODO:dataform、functions実行のための最小必要権限の追加
-    "roles/workflows.executions.create",
     "roles/logging.logWriter"
   ]
 }
