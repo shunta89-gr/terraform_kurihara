c270d053049f5bb40c1968537c887b93a0bc6d05
阿久津 健 <01044876@Y19MM284.local>
2023-09-21 19:31:52 +0900

    dataformモジュールの追加

---
+++ modules/dataform/main.tf
@@ -0,0 +1,32 @@
+resource "google_secret_manager_secret" "dataform_git" {
+    provider  = google-beta
+    secret_id = var.dataform_secret_id
+    
+    replication {
+        auto {}
+    }
+}
+
+resource "google_secret_manager_secret_version" "dataform_git_version" {
+    provider = google-beta
+    secret      = google_secret_manager_secret.dataform_git.id
+    secret_data = var.authentication_token 
+}
+
+resource "google_secret_manager_secret_iam_member" "member" {
+    provider = google-beta
+    secret_id = google_secret_manager_secret.dataform_git.secret_id
+    role      = "roles/secretmanager.secretAccessor"
+    member    = "serviceAccount:${google_service_account.dataform_sa.email}"
+}
+
+resource "google_dataform_repository" "repository" {
+    provider = google-beta
+    name = var.dataform_name
+    
+    git_remote_settings {
+        url                                 = var.github_url
+        default_branch                      = var.default_branch
+        authentication_token_secret_version = google_secret_manager_secret_version.dataform_git_version.id
+    }
+}
