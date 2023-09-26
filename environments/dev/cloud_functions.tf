bb03bca0a826053f5ff5bd70df0b618011e801f1
阿久津 健 <01044876@y19mm284.lan>
2023-09-26 16:39:15 +0900

    regionの値を変数を使用するよう修正

--- environments/dev/cloud_functions.tf
+++ environments/dev/cloud_functions.tf
@@ -4,8 +4,8 @@ module "cloud_functions" {
     source_dir           = "./cloud-functions/src"
     output_path          = "./cloud-functions/src.zip"
     bucket_name          = "kenkoukazoku-function-dev"
-    bucket_region        = "asia-northeast1"
-    function_region      = "asia-northeast1"
+    bucket_region        = var.region
+    function_region      = var.region
     function_name        = "kenkoukazoku-function"
     function_description = "rowデータの取り込み処理"
     function_runtime     = "python311"
