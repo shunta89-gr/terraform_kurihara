eb76456e24a9ac9d8e8ac34bd0106ab931061eb3
阿久津 健 <01044876@y19mm284.lan>
2023-11-02 16:47:07 +0900

    バッケト名をdev用に変更

--- environments/dev/storage.tf
+++ environments/dev/storage.tf
@@ -1,12 +1,12 @@
 module "data-sorce-bucket" {
     source = "../../modules/cloud_storage"
-    storage_name = "kenkoukazoku-rawdata"
+    storage_name = "kenkoukazoku-rawdata-dev"
     location = "asia-northeast1"
 }
 
 module "data-sorce-backup-bucket" {
     source = "../../modules/cloud_storage"
-    storage_name = "kenkoukazoku-rawdata-backup"
+    storage_name = "kenkoukazoku-rawdata-backup-dev"
     location = "asia-northeast1"
 }
 
