8c34a5d03a62358a03622e02fa116157f2fea77a
グエンティトゥイ ハン <01059138@ad.hproducts.co.jp>
2024-01-11 11:10:01 +0900

    notificaton_email_addressesの内容を修正

--- environments/prod/notifacation_channel.tf
+++ environments/prod/notifacation_channel.tf
@@ -2,8 +2,10 @@ module "notification_channel" {
   source                    = "../../modules/notification_channel"
   notification_channel_name = "tosashimizu-alert-channel"
   notification_email_addresses = [
-    "shunta.kurihara@growthdata.co.jp",
-    "michizo.ujihara@growthdata.co.jp",
-    "masaru.akutsu@hakuhodo.co.jp"
+    "masaru.akutsu@growthdata.co.jp",
+    "akira.aramaki@growthdata.co.jp",
+    "yusuke.nakata@growthdata.co.jp",
+    "hang.nguyenthithuy@growthdata.co.jp",
+    "riku.ichihara@growthdata.co.jp"
   ]
 }
